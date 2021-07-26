68fc14d00a5788e6e2afa83c3961377fcb195417
Jiaheng Lin <jiaheng.lin@shopee.com>
2021-07-27 00:29:05 +0800

    feat: bind event

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import { setState } from 'react';
 class Clock extends React.Component {
+  timerID: number | undefined;
   constructor(props) {
     super(props);
     this.state = { date: new Date() };
+    this.testClick = this.testClick.bind(this);
   }
   componentDidMount() {
     this.timerID = setInterval(
@@ -19,11 +22,15 @@ class Clock extends React.Component {
       date: new Date()
     });
   }
+  testClick(e) {
+    console.log(e);
+  }
   render() {
     return (
       <div>
         <h1>Hello, React!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        <h3 onClick={this.testClick}>点击一下</h3>
       </div>
     );
   }
